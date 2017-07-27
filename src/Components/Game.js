778b08c04b536ced2d547b61165fee03642ff9e4
Lee Elvin <lelvin@itim.com>
2017-07-27 09:27:39 +0100

    Nanites are now a BigNumber to avoid JS integer limit. The stored value is now "tenths of nanites" so that later on we can generate 0.1 nanite per second without the headache of decimal division

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
+import BigNumber from 'big-number';
 
 class Game extends Component {
 	constructor() {
 		super();
 
 		this.state = {
-			nanites: 0
+			naniteTenths: BigNumber(0)
 		};
 	}
 
-	handleButtonClick = () => this.setState({nanites: this.state.nanites + 1});
+	displayNaniteValue = () => {
+		const wholeNanites = BigNumber(this.state.naniteTenths).div(10);
+		return wholeNanites.val();
+	};
+
+	addNanites = tenthsToAdd => {
+		let newNanites = BigNumber(this.state.naniteTenths);
+		newNanites.add(tenthsToAdd);
+		this.setState({
+			naniteTenths: newNanites
+		});
+	};
+
+	handleButtonClick = () => {
+		this.addNanites(10);
+	};
 
 	render() {
 		return (
 			<div>
-				<h1>You have {this.state.nanites} nanites.</h1>
+				<h1>You have {this.displayNaniteValue()} nanites.</h1>
 				<button onClick={this.handleButtonClick}>Click to generate nanite</button>
 			</div>
 		);
