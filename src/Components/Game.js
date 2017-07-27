2a3c02fabda2867260264e9f1342076ad79db508
Lee Elvin <lelvin@itim.com>
2017-07-27 08:35:11 +0100

    Click a button, get a nanite

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			nanites: 0
+		};
+	}
+
+	handleButtonClick = () => this.setState({nanites: this.state.nanites + 1});
+
 	render() {
 		return (
 			<div>
-
+				<h1>You have {this.state.nanites} nanites.</h1>
+				<button onClick={this.handleButtonClick}>Click to generate nanite</button>
 			</div>
 		);
 	}
 }
+
+export default Game;
